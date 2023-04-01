6134d69bfb8add560b9c91a312d40a37518e0c68
dheepakg <sslvgdek@gmail.com>
2023-04-01 14:36:53 +0530

    Disabling log_decorator

--- src/log_decorator.py
+++ src/log_decorator.py
@@ -1,38 +1,38 @@
-import functools
-import logging
-import time
-from datetime import datetime
+# import functools
+# import logging
+# import time
+# from datetime import datetime
 
-from configParser import read_config_file
+# from configParser import read_config_file
 
-# To capture time taken
-
-
-config_logs = read_config_file()["logging"]
-now = datetime.now()
-# YYYYMMDD = str(now.strftime("%Y%m%d-%H%M"))
-YYYYMMDD = str(now.strftime("%Y%m%d"))
-
-logging.basicConfig(
-    level=config_logs["level"],
-    format=config_logs["format"],
-    datefmt=config_logs["date_format"],
-    filename="logs/PFDash-" + YYYYMMDD + ".log",
-    filemode="a",
-)
+# # To capture time taken
 
 
-def log_execution(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        logging.info(f"Executing {func.__name__}")
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        logging.info(
-            f"Finished execution of {func.__name__}. Took {round(end_time - start_time,5)} seconds to run "
-        )
+# config_logs = read_config_file()["logging"]
+# now = datetime.now()
+# # YYYYMMDD = str(now.strftime("%Y%m%d-%H%M"))
+# YYYYMMDD = str(now.strftime("%Y%m%d"))
 
-    return wrapper
+# logging.basicConfig(
+#     level=config_logs["level"],
+#     format=config_logs["format"],
+#     datefmt=config_logs["date_format"],
+#     filename="logs/PFDash-" + YYYYMMDD + ".log",
+#     filemode="a",
+# )
+
+
+# def log_execution(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         start_time = time.time()
+#         logging.info(f"Executing {func.__name__}")
+#         result = func(*args, **kwargs)
+#         end_time = time.time()
+#         logging.info(
+#             f"Finished execution of {func.__name__}. Took {round(end_time - start_time,5)} seconds to run "
+#         )
+
+#     return wrapper
 
 
