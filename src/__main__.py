e622746982f36e03fe671df657907ada8b2c968e
dheepakg <sslvgdek@gmail.com>
2023-04-01 13:26:43 +0530

    library in main file  is updated

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,18 @@
 from priceTrendCapture import priceCapture
-from accessConfig import read_config_file
+from configParser import read_config_file
 
 config_contents = read_config_file()
 
-hist_nav = priceCapture(config_contents["fund_list"])
+fund_details = config_contents["fund_details"]
+# hist_nav = priceCapture(config_contents["fund_details"])
 
-hist_nav.joinFundNAVs()
+# print(config_contents["fund_details"].keys())
+
+
+fund_dict = {fund_details[fund_name]['code'] : fund_details[fund_name]['name'] for fund_name in fund_details.keys() if fund_details[fund_name]['code']  }
+
+
+print(list(fund_dict.keys()))
+
+
+
